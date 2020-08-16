11a671a795732d84796410c09f6960841dc1b73b
Damien Pinna <damien.pinna@gmail.com>
2020-08-16 17:58:14 +0200

    connection à la base de donnée

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,32 @@
+require('dotenv').config()
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
+
+/* Connexion à la base de données MySQL */
+const connection = mysql.createConnection({
+   host     : process.env.DB_HOST,
+   user     : process.env.DB_USER,
+   password : process.env.DB_PASS,
+   database : process.env.DB_NAME
+});
+
+connection.connect(function(err) {
+   if (err) {
+   console.error('error connecting: ' + err.stack);
+   return;
+   }
+
+   console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM user', function (error, results, fields) {
+   if (error) throw error;
+   console.log(results);
+});
+
+connection.end();
 
 app.use('/api/test',(req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
