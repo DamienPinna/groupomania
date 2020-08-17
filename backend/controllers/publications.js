fb803760ebb4b6ae04689adee66e2b01ae28f01b
Damien Pinna <damien.pinna@gmail.com>
2020-08-17 16:26:36 +0200

    changement de la requête dans getAllPublications

--- backend/controllers/publications.js
+++ backend/controllers/publications.js
@@ -2,9 +2,15 @@ const db = require('../db');
 
 exports.getAllPublications = (req, res) => {
    db.connection();
-   db.instance.query('SELECT * FROM post', function (error, results, fields) {
+   db.instance.query(`SELECT postId, dateStamp, gifUrl, post.userId, login, role 
+                     FROM post
+                     INNER JOIN user
+                     ON post.userId = user.userId
+                     INNER JOIN role
+                     ON user.roleId = role.roleId;`,
+   function (error, results, fields) {
       if (error) throw error;
       res.status(200).json(results);
    });
-   db.disconnection(); 
+   db.disconnection();
 };
