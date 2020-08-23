52f89144324a8798dab39a99139fecaa3885e4d2
Damien Pinna <damien.pinna@gmail.com>
2020-08-23 12:44:19 +0200

    finalisation de la fonction signup

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,7 +1,7 @@
+const db = require('../db');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const passwordValidator = require('password-validator');
-const emailValidator = require("email-validator");
 
 const schema = new passwordValidator();
 
@@ -15,17 +15,20 @@ schema
 .has().symbols()
 
 
-exports.signup = (req, res) => {
-   const email = req.body.email;
+exports.signup = async (req, res) => {
    const password = req.body.password;
 
-   if (emailValidator.validate(email) && schema.validate(password)) {
-      res.status(200).json({message: "ok"});
-   } else if (!emailValidator.validate(email) && !schema.validate(password)) {
-      res.status(500).json({message: "L'adresse email est invalide et le mot de passe doit contenir au moins 8 caractères dont 1 majuscule, 1 minuscule, 1 nombre, 1 caractère spécial et pas d'espace."});
-   } else if (!emailValidator.validate(email)) {
-      res.status(500).json({message: 'Adresse email invalide.'});
+   if (schema.validate(password)) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      db.connection();
+      db.instance.query(`INSERT INTO user (login,password)
+                        VALUES ("${req.body.login}","${hashedPassword}");`,
+      function (error, results, fields) {
+         if (error) throw error;
+         res.status(200).json({message: 'Utilisateur enregistré'});
+         db.disconnection();
+      });
    } else {
       res.status(500).json({message: "Le mot de passe doit contenir au moins 8 caractères dont 1 majuscule, 1 minuscule, 1 nombre, 1 caractère spécial et pas d'espace."});
-   }
+   };
 };
