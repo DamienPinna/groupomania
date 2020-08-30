166597496aac889fb3952984a94b3c7b9519f4bf
Damien Pinna <damien.pinna@gmail.com>
2020-08-30 12:11:58 +0200

    ajout de gardes au niveau de vue router

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,18 @@ import GifSelect from '../views/Gif-select.vue'
 
 Vue.use(VueRouter)
 
+const guardMyRoute = (to, from, next) => {
+   const tokenFromStorage = localStorage.getItem('token');
+   if (tokenFromStorage) next();
+   else next('/');
+}
+
 const routes = [
    {path: '/', name: 'Login', component: Login},
    {path: '/signup', name: 'Signup', component: Signup},
-   {path: '/home', name: 'Home', component: Home},
-   {path: '/add-gif', name: 'Add-gif', component: AddGif},
-   {path: '/gif-select', name: 'gif-select', component: GifSelect}
+   {path: '/home', name: 'Home', component: Home, beforeEnter: guardMyRoute},
+   {path: '/add-gif', name: 'Add-gif', component: AddGif, beforeEnter: guardMyRoute},
+   {path: '/gif-select', name: 'gif-select', component: GifSelect, beforeEnter: guardMyRoute}
 ]
 
 const router = new VueRouter({
